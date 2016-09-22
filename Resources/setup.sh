d3a3d36e4f9aa11b2ca9db491fb2145a4ada5dc4
Ole-André Rodlie <olear@dracolinux.org>
2016-09-22 21:55:01 +0200

    fix installer

--- Resources/setup.sh
+++ Resources/setup.sh
@@ -9,7 +9,7 @@ RESOURCES="$CWD/Resources"
 PACKAGES="$TMP/packages"
 DOMAIN="natron.community.plugins"
 
-BINARY="NCP-${VERSION}"
+BINARY="NCP-${VERSION}-${OS}-${BIT}"
 if [ "$OS" != "Linux" ] && [ "$OS" != "Darwin" ]; then
   BINARY="${BINARY}.exe"
   OS="Windows"
@@ -49,9 +49,17 @@ for PLUGINPATH in $PLUGINS; do
   PLUGINNAME=`echo $PLUGINPATH | sed 's@.*/@@'`
   mkdir -p "$PACKAGES"/${DOMAIN}.$PLUGINNAME/{data,meta} || exit 1
   cp "$PLUGINPATH"/${PLUGINNAME}.p* "$PACKAGES"/${DOMAIN}.$PLUGINNAME/data/ || exit 1
-  cat "$PLUGINPATH"/${PLUGINNAME}.xml | sed 's/@DATE@/'${DATE}'/' > "$PACKAGES"/${DOMAIN}.$PLUGINNAME/meta/package.xml || exit 1
+  cat "$PLUGINPATH"/${PLUGINNAME}.xml | sed 's/@DATE@/'${DATE}'/;s#</Package>#<Dependencies>'${DOMAIN}'.core</Dependencies></Package>#' > "$PACKAGES"/${DOMAIN}.$PLUGINNAME/meta/package.xml || exit 1
   cat "$RESOURCES"/package.qs > "$PACKAGES"/${DOMAIN}.$PLUGINNAME/meta/installscript.qs || exit 1
+  cp "$CWD"/Licenses/GPL-2.0 "$PACKAGES"/${DOMAIN}.$PLUGINNAME/meta/ || exit 1
+  cp "$CWD"/Licenses/CC-BY-2.0 "$PACKAGES"/${DOMAIN}.$PLUGINNAME/meta/ || exit 1
 done
+
+mkdir -p "$PACKAGES"/${DOMAIN}.core/{data,meta} || exit 1
+cat "$RESOURCES"/core.xml | sed 's/@DATE@/'${DATE}'/' > "$PACKAGES"/${DOMAIN}.core/meta/package.xml || exit 1
+cat "$RESOURCES"/package.qs > "$PACKAGES"/${DOMAIN}.core/meta/installscript.qs || exit 1
+cp "$CWD"/Licenses/GPL-2.0 "$PACKAGES"/${DOMAIN}.core/meta/ || exit 1
+cp "$CWD"/Licenses/CC-BY-2.0 "$PACKAGES"/${DOMAIN}.core/meta/ || exit 1
 
 if [ -f "$RESOURCES"/${OS}.xml ]; then
   PATH="$RESOURCES"/utils/${OS}${BIT}:$PATH binarycreator -v -f -p "$PACKAGES" -c "$RESOURCES"/${OS}.xml "$BINARY"
