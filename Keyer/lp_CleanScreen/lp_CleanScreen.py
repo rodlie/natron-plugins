c8d505c25ade34e9fa302180d7d3b988a914a6e1
Johannes Wuensch <j.wuensch@protonmail.com>
2016-09-30 21:35:32 +0200

    add Icon for lp_CleanScreen + update description

--- Keyer/lp_CleanScreen/lp_CleanScreen.py
+++ Keyer/lp_CleanScreen/lp_CleanScreen.py
@@ -24,11 +24,14 @@ def getLabel():
 def getVersion():
     return 1
 
+def getIconPath():
+    return "lp_CleanScreen.png"
+
 def getGrouping():
     return "Keyer"
 
 def getPluginDescription():
-    return "CleanScreen helps you to even out chroma-background (R, G or B) with the help of a cleanplate.\n\nNote that while you can hook up generated ones, it usually makes more sense to only use the real thing :)\n"
+    return "CleanScreen helps you to even out chroma-background (R, G or B) with the help of a cleanplate.\n\nNote that while you can hook up generated ones, it usually makes more sense to only use real clean plates. For good results the clean plate should be as exact as possible, so it works best with locked cameras. Moving cloth screens won't do the trick...  \n"
 
 def createInstance(app,group):
     # Create all nodes in the group
