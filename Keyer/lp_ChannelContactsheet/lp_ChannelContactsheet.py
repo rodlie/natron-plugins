0f5b2f3cccca95a8111dea076a7c83f0055ea22c
sozap <sozaps@gmail.com>
2017-04-15 16:03:59 +0200

    lp_ChannelContactsheet and lp_SimpleKeyer was not in the community plugin folder

--- Keyer/lp_ChannelContactsheet/lp_ChannelContactsheet.py
+++ Keyer/lp_ChannelContactsheet/lp_ChannelContactsheet.py
@@ -25,7 +25,7 @@ def getVersion():
     return 1
 
 def getGrouping():
-    return "Keyer"
+    return "Community/Keyer"
 
 def getPluginDescription():
     return "lp_ChannelContactsheet\n\nv 1\n\nKeyer\n\nDisplays an input image in different colourspaces to find a suitable key. Also lets you choose and key from such a channel right away :)\n\nHOW TO USE IT\nConnect an image and look out for a good, high-contrast channel you want to work from. Then, choose the suitable colourspace and channel you want to key from and switch the output from \"Contact Sheet\" to \"Keyer\". You can now use black point, white point, range controls etc. to adjust the key as needed. You can also invert the generated matte and premultiply your result right away.\n\nHOW DOES IT WORK\nThe input-channel gets reformatted to a smaller version of itself and then split into various different colourspaces, arranged into a grid. For the Keyer-mode, the same happens without the reformatting, with added controls for adjusting the key; the same controls are also found in lp_SimpleKeyer.\n\nThis tool is based on Tyler Lockards great Nuke Gizmo: http://www.nukepedia.com/gizmos/keyer/channelcontactsheet"
