634f37f22b024781d869c8c1f8815da8420df217
fabiof17 <fabiof17pro@gmail.com>
2018-03-04 19:50:22 +0100

    New GLSL scripts added.

---
+++ Shadertoy/CPGP_FractalCell.frag.glsl
@@ -0,0 +1,144 @@
+//
+//
+//                          MMMMMMMMMMMMMMMMMMMMMMMMMMMM
+//                        MM.                          .MM
+//                       MM.  .MMMMMMMMMMMMMMMMMMMMMM.  .MM
+//                      MM.  .MMMMMMMMMMMMMMMMMMMMMMMM.  .MM
+//                     MM.  .MMMM        MMMMMMM    MMM.  .MM
+//                    MM.  .MMM           MMMMMM     MMM.  .MM
+//                   MM.  .MmM              MMMM      MMM.  .MM
+//                  MM.  .MMM                 MM       MMM.  .MM
+//                 MM.  .MMM                   M        MMM.  .MM
+//                MM.  .MMM                              MMM.  .MM
+//                 MM.  .MMM                            MMM.  .MM
+//                  MM.  .MMM       M                  MMM.  .MM
+//                   MM.  .MMM      MM                MMM.  .MM
+//                    MM.  .MMM     MMM              MMM.  .MM
+//                     MM.  .MMM    MMMM            MMM.  .MM
+//                      MM.  .MMMMMMMMMMMMMMMMMMMMMMMM.  .MM
+//                       MM.  .MMMMMMMMMMMMMMMMMMMMMM.  .MM
+//                        MM.                          .MM
+//                          MMMMMMMMMMMMMMMMMMMMMMMMMMMM
+//
+//
+//
+//
+// Adaptation pour Natron par F. Fernandez
+// Code original : CPGP_FractalCell Matchbox pour Autodesk Flame
+
+// Adapted to Natron by F.Fernandez
+// Original code : CPGP_FractalCell Matchbox for Autodesk Flame
+
+
+// iChannel0: Source, filter = nearest, wrap = clamp
+// BBox: iChannel0
+
+
+
+uniform float paramSpeed; // speed : (speed), min = 0., max = 1000.
+uniform bool UseLigthModulation; // use light modulation : (use light modulation)
+
+
+
+//CBS
+//Parallax scrolling fractal galaxy.
+//Inspired by JoshP's Simplicity shader: https://www.shadertoy.com/view/lslGWr
+
+// http://www.fractalforums.com/new-theories-and-research/very-simple-formula-for-fractal-patterns/
+// Ported from ShaderToys.com by redexe@gmail.com
+
+float field(in vec3 p,float s) {
+	float strength = 7. + .03 * log(1.e-6 + fract(sin(iTime*paramSpeed/100.0) * 4373.11));
+	float accum = s/4.;
+	float prev = 0.;
+	float tw = 0.;
+	for (int i = 0; i < 26; ++i) {
+		float mag = dot(p, p);
+		p = abs(p) / mag + vec3(-.5, -.4, -1.5);
+		float w = exp(-float(i) / 7.);
+		accum += w * exp(-strength * pow(abs(mag - prev), 2.2));
+		tw += w;
+		prev = mag;
+	}
+	return max(0., 5. * accum / tw - .7);
+}
+
+// Less iterations for second layer
+float field2(in vec3 p, float s) {
+	float strength = 7. + .03 * log(1.e-6 + fract(sin(iTime*paramSpeed/100.0) * 4373.11));
+	float accum = s/4.;
+	float prev = 0.;
+	float tw = 0.;
+	for (int i = 0; i < 18; ++i) {
+		float mag = dot(p, p);
+		p = abs(p) / mag + vec3(-.5, -.4, -1.5);
+		float w = exp(-float(i) / 7.);
+		accum += w * exp(-strength * pow(abs(mag - prev), 2.2));
+		tw += w;
+		prev = mag;
+	}
+	return max(0., 5. * accum / tw - .7);
+}
+
+vec3 nrand3( vec2 co )
+{
+	vec3 a = fract( cos( co.x*8.3e-3 + co.y )*vec3(1.3e5, 4.7e5, 2.9e5) );
+	vec3 b = fract( sin( co.x*0.3e-3 + co.y )*vec3(8.1e5, 1.0e5, 0.1e5) );
+	vec3 c = mix(a, b, 0.5);
+	return c;
+}
+
+//-----------------------------------------------------------------------------
+// Main functions
+//-----------------------------------------------------------------------------
+
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+    
+    vec2 uv = 2. * fragCoord.xy / iResolution.xy - 1.;
+	vec2 uvs = uv * iResolution.xy / max(iResolution.x, iResolution.y);
+	vec3 p = vec3(uvs / 4., 0) + vec3(1., -1.3, 0.);
+	p += .2 * vec3(sin(iTime*paramSpeed/100.0 / 16.), sin(iTime*paramSpeed/100.0 / 12.),  sin(iTime*paramSpeed/100.0 / 128.));
+	
+	float freqs[4];
+	//There is no sound here, so a texture sample has been put in its place, it can be removed....
+    if(UseLigthModulation)
+    {
+	freqs[0] = texture2D( iChannel0, vec2( 0.01, 0.25 ) ).x;
+	freqs[1] = texture2D( iChannel0, vec2( 0.07, 0.25 ) ).x;
+	freqs[2] = texture2D( iChannel0, vec2( 0.15, 0.25 ) ).x;
+	freqs[3] = texture2D( iChannel0, vec2( 0.30, 0.25 ) ).x;
+    }
+    else
+    {
+    freqs[0] = 0.5;
+    freqs[1] = 0.5;
+    freqs[2] = 0.5;
+    freqs[3] = 0.5;
+    }
+	
+	float t = field(p,freqs[2]);
+	float v = (1. - exp((abs(uv.x) - 1.) * 6.)) * (1. - exp((abs(uv.y) - 1.) * 6.));
+	
+    //Second Layer
+	vec3 p2 = vec3(uvs / (4.+sin(iTime*paramSpeed/100.0*0.11)*0.2+0.2+sin(iTime*paramSpeed/100.0*0.15)*0.3+0.4), 1.5) + vec3(2., -1.3, -1.);
+	p2 += 0.25 * vec3(sin(iTime*paramSpeed/100.0 / 16.), sin(iTime*paramSpeed/100.0 / 12.),  sin(iTime*paramSpeed/100.0 / 128.));
+	float t2 = field2(p2,freqs[3]);
+	vec4 c2 = mix(.4, 1., v) * vec4(1.3 * t2 * t2 * t2 ,1.8  * t2 * t2 , t2* freqs[0], t2);
+	
+	
+	//Let's add some stars
+	//Thanks to http://glsl.heroku.com/e#6904.0
+	vec2 seed = p.xy * 2.0;
+	seed = floor(seed * iResolution.x);
+	vec3 rnd = nrand3( seed );
+	vec4 starcolor = vec4(pow(rnd.y,40.0));
+	
+	//Second Layer
+	vec2 seed2 = p2.xy * 2.0;
+	seed2 = floor(seed2 * iResolution.x);
+	vec3 rnd2 = nrand3( seed2 );
+	starcolor += vec4(pow(rnd2.y,40.0));
+	
+	fragColor = mix(freqs[3]-.3, 1., v) * vec4(1.5*freqs[2] * t * t* t , 1.2*freqs[1] * t * t, freqs[3]*t, 1.0)+c2+starcolor;
+}
---
+++ Shadertoy/Crok_exposure.frag.glsl
@@ -0,0 +1,201 @@
+//
+//
+//                          MMMMMMMMMMMMMMMMMMMMMMMMMMMM
+//                        MM.                          .MM
+//                       MM.  .MMMMMMMMMMMMMMMMMMMMMM.  .MM
+//                      MM.  .MMMMMMMMMMMMMMMMMMMMMMMM.  .MM
+//                     MM.  .MMMM        MMMMMMM    MMM.  .MM
+//                    MM.  .MMM           MMMMMM     MMM.  .MM
+//                   MM.  .MmM              MMMM      MMM.  .MM
+//                  MM.  .MMM                 MM       MMM.  .MM
+//                 MM.  .MMM                   M        MMM.  .MM
+//                MM.  .MMM                              MMM.  .MM
+//                 MM.  .MMM                            MMM.  .MM
+//                  MM.  .MMM       M                  MMM.  .MM
+//                   MM.  .MMM      MM                MMM.  .MM
+//                    MM.  .MMM     MMM              MMM.  .MM
+//                     MM.  .MMM    MMMM            MMM.  .MM
+//                      MM.  .MMMMMMMMMMMMMMMMMMMMMMMM.  .MM
+//                       MM.  .MMMMMMMMMMMMMMMMMMMMMM.  .MM
+//                        MM.                          .MM
+//                          MMMMMMMMMMMMMMMMMMMMMMMMMMMM
+//
+//
+//
+//
+// Adaptation pour Natron par F. Fernandez
+// Code original : crok_exposure Matchbox pour Autodesk Flame
+
+// Adapted to Natron by F.Fernandez
+// Original code : crok_exposure Matchbox for Autodesk Flame
+
+
+// iChannel0: Source, filter = nearest, wrap = clamp
+// BBox: iChannel0
+
+
+uniform float expo_o = 0; // global expo : (global exposition), min = 1., max = 50.
+uniform float expo_r = 0; // R expo : (red exposition), min = 1., max = 50.
+uniform float expo_g = 0; // G expo : (green exposition), min = 1., max = 50.
+uniform float expo_b = 0; // B expo : (blue exposition), min = 1., max = 50.
+
+uniform float contrast_all = 0.25; // global contrast : (global contrast), min = 0, max = 10.
+uniform float contrast_r = 1; // R contrast : (red contrast), min = 0., max = 50.
+uniform float contrast_g = 1; // G contrast : (green contrast), min = 0., max = 50.
+uniform float contrast_b = 1; // B contrast : (blue contrast), min = 0., max = 50.
+
+uniform float pivot_all = 1; // gloabl pivot : (pivot all), min = 0, max = 10.
+uniform float pivot_r = 0; // R pivot : (red pivot), min = 0., max = 50.
+uniform float pivot_g = 0; // G pivot : (green pivot), min = 0., max = 50.
+uniform float pivot_b = 0; // B pivot : (blue pivot), min = 0., max = 50.
+
+uniform float sat_gregPhase = 1; // phase : (phase), min = 0, max = 10.
+uniform int encoding = 0; // colorspace : (colorspace), min = 0, max = 2.
+
+float ga = 17.5;
+float br = 1.74;
+
+// contrast by  Miles
+vec3 adjust_contrast(vec3 col, vec4 con, vec4 piv)
+{
+	vec3 c = con.rgb * vec3(con.a);
+	vec3 a_pivot = piv.rgb * vec3(piv.a);
+	col = (1.0 - c.rgb) * a_pivot + c.rgb * col;
+	return col;
+}
+
+// Algorithm from Chapter 16 of OpenGL Shading Language
+vec3 saturation(vec3 rgb, float adjustment)
+{
+    const vec3 W = vec3(0.2125, 0.7154, 0.0721);
+    vec3 intensity = vec3(dot(rgb, W));
+    return mix(intensity, rgb, adjustment);
+}
+
+// rec709 conversion routine by Miles
+vec3 from_rec709(vec3 col)
+{
+	if (col.r < .081) {
+		col.r /= 4.5;
+	} else {
+		col.r = pow((col.r +.099)/ 1.099, 1 / .45);
+	}
+	if (col.g < .081) {
+		col.g /= 4.5;
+	} else {
+		col.g = pow((col.g +.099)/ 1.099, 1 / .45);
+	}
+	if (col.b < .081) {
+		col.b /= 4.5;
+	} else {
+		col.b = pow((col.b +.099)/ 1.099, 1 / .45);
+	}
+	return col;
+}
+
+vec3 to_rec709(vec3 col)
+{
+    if (col.r < .018) {
+        col.r *= 4.5;
+    } else if (col.r >= 0.0) {
+        col.r = (1.099 * pow(col.r, .45)) - .099;
+    }
+    if (col.g < .018) {
+        col.g *= 4.5;
+    } else if (col.g >= 0.0) {
+        col.g = (1.099 * pow(col.g, .45)) - .099;
+    }
+    if (col.b < .018) {
+        col.b *= 4.5;
+    } else if (col.b >= 0.0) {
+        col.b = (1.099 * pow(col.b, .45)) - .099;
+    }
+    return col;
+}
+
+vec3 from_log(vec3 col)
+{
+	if (col.r >= 0.0) {
+		col.r = pow((col.r + br), ga);
+	}
+	if (col.g >= 0.0) {
+		col.g = pow((col.g + br), ga);
+	}
+	if (col.b >= 0.0) {
+		col.b = pow((col.b + br), ga);
+	}
+	return col;
+}
+
+vec3 to_log(vec3 col)
+{
+    if (col.r >= 0.0) {
+    	col.r = (pow(col.r, 1.0 / ga)) - br;
+	}
+    if (col.g >= 0.0) {
+    	col.g = (pow(col.g, 1.0 / ga)) - br;
+	}
+    if (col.b >= 0.0) {
+    	col.b = (pow(col.b, 1.0 / ga)) - br;
+	}
+    return col;
+}
+
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+  vec2 uv = fragCoord.xy / iResolution.xy;
+	vec3 col = texture2D(iChannel0, uv).rgb;
+	vec3 contrast = vec3(contrast_r, contrast_g, contrast_b);
+	vec3 c_pivot = vec3(pivot_r, pivot_g, pivot_b);
+
+	// Scene linear exposure
+	if ( encoding == 0 )
+	{
+		// contrast stuff by Miles
+		col = adjust_contrast(col, vec4(contrast, contrast_all), vec4(c_pivot, pivot_all));
+		// overall exposure adjustemnt
+		col = col * pow(2.0, expo_o);
+		// single rgb exposure adjustment
+		col.r = col.r * pow(2.0, expo_r);
+		col.g = col.g * pow(2.0, expo_g);
+		col.b = col.b * pow(2.0, expo_b);
+		// desaturation Greg-Paul style
+		col = saturation(col, 1.0 - expo_o * 0.01 * sat_gregPhase);
+}
+
+	// video  / Rec 709 exposure
+	if ( encoding == 1)
+	{
+		col = from_rec709(col);
+		// contrast stuff by Miles
+		col = adjust_contrast(col, vec4(contrast, contrast_all), vec4(c_pivot, pivot_all));
+		// overall exposure adjustemnt
+		col = col * pow(2.0, expo_o);
+		// single rgb exposure adjustment
+		col.r = col.r * pow(2.0, expo_r);
+		col.g = col.g * pow(2.0, expo_g);
+		col.b = col.b * pow(2.0, expo_b);
+		// desaturation Greg-Paul style
+		col = saturation(col, 1.0 - expo_o * 0.01 * sat_gregPhase);
+		col = to_rec709(col);
+	}
+
+	// Logarithmic exposure
+	if ( encoding == 2)
+	{
+		col = from_log(col);
+		// overall exposure adjustemnt
+		col = col * pow(2.0, expo_o);
+		// single rgb exposure adjustment
+		col.r = col.r * pow(2.0, expo_r);
+		col.g = col.g * pow(2.0, expo_g);
+		col.b = col.b * pow(2.0, expo_b);
+		// desaturation Greg-Paul style
+		col = saturation(col, 1.0 - expo_o * 0.01 * sat_gregPhase);
+		col = to_log(col);
+		// contrast stuff by Miles
+		col = adjust_contrast(col, vec4(contrast, contrast_all), vec4(c_pivot, pivot_all));
+	}
+
+	fragColor = vec4(col, 1.0);
+}
