a9dd974167d7a4b37f53907d7c6dcf2dd9c38b6f
Frédéric Devernay <frederic.devernay@m4x.org>
2020-04-07 22:00:53 -0700

    Merge_GL.py: fix channel sampling + format source
    - iChannelOffset and iChannelResolution were missing, as in:
      vec4 s = texture2D(iChannel1, (fragCoord.xy-iChannelOffset[1].xy)/iChannelResolution[1].xy);
    
    - use multiline Python string to make the code more readable

--- GLSL/Merge/Merge_GL/Merge_GL.py
+++ GLSL/Merge/Merge_GL/Merge_GL.py
@@ -669,7 +669,410 @@ def createInstance(app,group):
 
     param = lastNode.getParam("imageShaderSource")
     if param is not None:
-        param.setValue("//                                                \n//                                                  \n//                          MMMMMMMMMMMMMMMMMMMMMMMMMMMM                                        \n//                        MM.                          .MM                                \n//                       MM.  .MMMMMMMMMMMMMMMMMMMMMM.  .MM                  \n//                      MM.  .MMMMMMMMMMMMMMMMMMMMMMMM.  .MM     \n//                     MM.  .MMMM        MMMMMMM    MMM.  .MM\n//                    MM.  .MMM           MMMMMM     MMM.  .MM\n//                   MM.  .MmM              MMMM      MMM.  .MM\n//                  MM.  .MMM                 MM       MMM.  .MM\n//                 MM.  .MMM                   M        MMM.  .MM\n//                MM.  .MMM                              MMM.  .MM\n//                 MM.  .MMM                            MMM.  .MM\n//                  MM.  .MMM       M                  MMM.  .MM\n//                   MM.  .MMM      MM                MMM.  .MM\n//                    MM.  .MMM     MMM              MMM.  .MM  \n//                     MM.  .MMM    MMMM            MMM.  .MM    \n//                      MM.  .MMMMMMMMMMMMMMMMMMMMMMMM.  .MM      \n//                       MM.  .MMMMMMMMMMMMMMMMMMMMMM.  .MM            \n//                        MM.                          .MM                 \n//                          MMMMMMMMMMMMMMMMMMMMMMMMMMMM                                                      \n//                                                                \n//\n// Adaptation pour Natron par F. Fernandez\n// Code original : crok_logicop Matchbox pour Autodesk Flame\n//\n// Adapted to Natron by F.Fernandez\n// Original code : crok_logicop Matchbox for Autodesk Flame\n//\n//****************************************************************************/\n// \n// Filename: logicOPS_3vis.glsl\n// Author: Eric Pouliot\n//\n// Copyright (c) 2013 3vis, Inc.\n//\n//*****************************************************************************/\n//\n//\tLicense Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.\n//\t\n//\t25 of the layer blending modes from Photoshop.\n//\t\n//\tThe ones I couldn\'t figure out are from Nvidia\'s advanced blend equations extension spec -\n//\thttp://www.opengl.org/registry/specs/NV/blend_equation_advanced.txt\n//\t\n//\t~bj.2013\n//\n//*****************************************************************************/\n\n// iChannel0: B, filter = nearest\n// iChannel1: A, filter = nearest\n// iChannel1: Modulate, filter = nearest\n// BBox: iChannel0\n\n\n\nuniform int blendingMode = 17; // Operation : (blending mode), min=0., max=25.\nuniform float opacity = 1; // Mix : (mix), min=0., max=1.\n\nuniform bool Ar = true; // R\nuniform bool Ag = true; // G\nuniform bool Ab = true; // B\nuniform bool Aa = true; // A\n\nuniform bool Br = true; // R\nuniform bool Bg = true; // G\nuniform bool Bb = true; // B\nuniform bool Ba = true; // A\n\n\n//********************* fonctions ***********************//\n\nfloat overlay( float s, float d )\n{\n\treturn (d < 0.5) ? 2.0 * s * d : 1.0 - 2.0 * (1.0 - s) * (1.0 - d);\n}\n\nfloat softLight( float s, float d )\n{\n\treturn (s < 0.5) ? d - (1.0 - 2.0 * s) * d * (1.0 - d) \n\t\t: (d < 0.25) ? d + (2.0 * s - 1.0) * d * ((16.0 * d - 12.0) * d + 3.0) \n\t\t\t\t\t : d + (2.0 * s - 1.0) * (sqrt(d) - d);\n}\n\nfloat hardLight( float s, float d )\n{\n\treturn (s < 0.5) ? 2.0 * s * d : 1.0 - 2.0 * (1.0 - s) * (1.0 - d);\n}\n\nfloat vividLight( float s, float d )\n{\n\treturn (s < 0.5) ? 1.0 - (1.0 - d) / (2.0 * s) : d / (2.0 * (1.0 - s));\n}\n\nfloat pinLight( float s, float d )\n{\n\treturn (2.0 * s - 1.0 > d) ? 2.0 * s - 1.0 : (s < 0.5 * d) ? 2.0 * s : d;\n}\n\n\n\n//****************** modes de fusion ********************//\n\nvec4 colorBurn( vec4 s, vec4 d )\n{\n\treturn 1.0 - (1.0 - d) / s;\n}\n\nvec4 colorDodge( vec4 s, vec4 d )\n{\n\treturn d / (1.0 - s);\n}\n\nvec4 darken( vec4 s, vec4 d )\n{\n\tvec4 c;\n\tc = min(s,d);\n\treturn c;\n}\n\nvec4 difference( vec4 s, vec4 d )\n{\n\treturn abs(d - s);\n}\n\nvec4 darkerColor( vec4 s, vec4 d )\n{\n\treturn (s.x + s.y + s.z < d.x + d.y + d.z) ? s : d;\n}\n\nvec4 divide( vec4 s, vec4 d )\n{\n\treturn s / d;\n}\n\nvec4 exclusion( vec4 s, vec4 d )\n{\n\treturn s + d - 2.0 * s * d;\n}\n\nvec4 hardLight( vec4 s, vec4 d )\n{\n\tvec4 c;\n\tc.x = hardLight(s.x,d.x);\n\tc.y = hardLight(s.y,d.y);\n\tc.z = hardLight(s.z,d.z);\n\treturn c;\n}\n\nvec4 hardMix( vec4 s, vec4 d )\n{\n\treturn floor(s + d);\n}\n\nvec4 lighten( vec4 s, vec4 d )\n{\n\treturn max(s,d);\n}\n\nvec4 lighterColor( vec4 s, vec4 d )\n{\n\treturn (s.x + s.y + s.z > d.x + d.y + d.z) ? s : d;\n}\n\nvec4 linearBurn( vec4 s, vec4 d )\n{\n\treturn s + d - 1.0;\n}\n\nvec4 linearDodge( vec4 s, vec4 d )\n{\n\treturn s + d;\n}\n\n\nvec4 linearLight( vec4 s, vec4 d )\n{\n\treturn 2.0 * s + d - 1.0;\n}\n\nvec4 screen( vec4 s, vec4 d )\n{\n\treturn s + d - s * d;\n}\n\nvec4 screenMode( vec4 s, vec4 d )\n{\n\tvec4 c;\n\tc = 1 - (1 - s) * (1 - d);\n\treturn c;\n}\n\nvec4 matte( vec4 s, vec4 d)\n{\n\tvec4 c = vec4(0,0,0,0);\n\tc = (s*s.a) + (d*(1-s.a));\n\tc.a = s.a;\n\treturn c;\n}\n\nvec4 multiply( vec4 s, vec4 d )\n{\n\treturn s*d;\n}\n\nvec4 overlay( vec4 s, vec4 d )\n{\n\tvec4 c;\n\tc.x = overlay(s.x,d.x);\n\tc.y = overlay(s.y,d.y);\n\tc.z = overlay(s.z,d.z);\n\treturn c;\n}\n\nvec4 pinLight( vec4 s, vec4 d )\n{\n\tvec4 c;\n\tc.x = pinLight(s.x,d.x);\n\tc.y = pinLight(s.y,d.y);\n\tc.z = pinLight(s.z,d.z);\n\treturn c;\n}\n\nvec4 plus( vec4 s, vec4 d )\n{\n\treturn s + d;\n}\n\nvec4 softLight( vec4 s, vec4 d )\n{\n\tvec4 c;\n\tc.x = softLight(s.x,d.x);\n\tc.y = softLight(s.y,d.y);\n\tc.z = softLight(s.z,d.z);\n\treturn c;\n}\n\nvec4 spotlightBlend( vec4 s, vec4 d )\n{\n\treturn d*s+d;\n}\n\nvec4 substract( vec4 s, vec4 d )\n{\n\treturn s - d;\n}\n\nvec4 vividLight( vec4 s, vec4 d )\n{\n\tvec4 c;\n\tc.x = vividLight(s.x,d.x);\n\tc.y = vividLight(s.y,d.y);\n\tc.z = vividLight(s.z,d.z);\n\treturn c;\n}\n\nvec4 luminosity( vec4 s, vec4 d )\n{\n\tfloat dLum = dot(d, vec4(0.3, 0.59, 0.11,0));\n\tfloat sLum = dot(s, vec4(0.3, 0.59, 0.11,0));\n\tfloat lum = sLum - dLum;\n\tvec4 c = d + lum;\n\tfloat minC = min(min(c.x, c.y), c.z);\n\tfloat maxC = max(max(c.x, c.y), c.z);\n\tif(minC < 0.0) return sLum + ((c - sLum) * sLum) / (sLum - minC);\n\telse if(maxC > 1.0) return sLum + ((c - sLum) * (1.0 - sLum)) / (maxC - sLum);\n\telse return c;\n}\n\nvec4 over( vec4 s, vec4 d )\n{\n\tvec4 c;\n\tc = s + ( d*(1-s.a) );\n\treturn c;\n}\n\n\n\n\n\nvoid mainImage( out vec4 fragColor, in vec2 fragCoord )\n{\n\n\tvec2 uv = fragCoord.xy / iResolution.xy;\n\n\t// source texture (upper layer)\n\tvec4 s = texture2D(iChannel1, uv);\n\t\n\t// destination texture (lower layer)\n\tvec4 d = texture2D(iChannel0, uv);\n\n\t// mask texture (mask entry)\n\tvec4 mask = texture2D(iChannel2, uv);\n\n\tvec4 c = vec4(0.0);\n\n\n\t// ########### selection des couches ########### //\n\n\tif(Ar == false)\n\t\ts.r = 0;\n\n\tif(Ag == false)\n\t\ts.g = 0;\n\n\tif(Ab == false)\n\t\ts.b = 0;\n\n\tif(Aa == false)\n\t\ts.a = 0;\n\n\tif(Br == false)\n\t\td.r = 0;\n\n\tif(Bg == false)\n\t\td.g = 0;\n\n\tif(Bb == false)\n\t\td.b = 0;\n\n\tif(Ba == false)\n\t\td.a = 0;\n\n\t// ########### selection des modes de fusion ########### //\n\n\tif( blendingMode == 0)\n\t   \tc =  c + colorBurn(s,d);\n\n\telse if( blendingMode == 1)\n\t   \tc =  c + colorDodge(s,d);\n\n\telse if( blendingMode == 2)\n\t   \tc =  c + darken(s,d);\n\n\telse if(blendingMode == 3)\n\t\tc =  c + darkerColor(s,d);\t\t\n\n\telse if(blendingMode == 4)\n\t\tc =  c + difference(s,d);\n\n\telse if(blendingMode == 5)\n\t\tc =  c + divide(s,d);\n\n\telse if(blendingMode == 6)\n\t\tc =  c + exclusion(s,d);\n\n\telse if(blendingMode == 7)\n\t\tc =  c + hardLight(s,d);\n\n\telse if(blendingMode == 8)\n\t\tc =  c + hardMix(s,d);\n\n\telse if(blendingMode == 9)\n\t\tc =  c + lighten(s,d);\t\t\n\n\telse if(blendingMode == 10)\n\t\tc =  c + lighterColor(s,d);\n\n\telse if(blendingMode == 11)\n\t\tc =  c + linearBurn(s,d);\n\n\telse if(blendingMode == 12)\n\t\tc =  c + linearDodge(s,d);\n\n\telse if(blendingMode == 13)\n\t\tc =  c + linearLight(s,d);\t\t\n\n\telse if(blendingMode == 14)\n\t\tc =  c + luminosity(s,d);\n\n\telse if(blendingMode == 15)\n\t\tc =  c + matte(s,d);\n\n\telse if(blendingMode == 16)\n\t\tc =  c + multiply(s,d);\n\n\telse if(blendingMode == 17)\n\t\tc =  c + over(s,d);\n\n\telse if(blendingMode == 18)\n\t\tc =  c + overlay(s,d);\n\n\telse if(blendingMode == 19)\n\t\tc =  c + pinLight(s,d);\n\n\telse if(blendingMode == 20)\n\t\tc =  c + plus(s,d);\n\n\telse if(blendingMode == 21)\n\t\tc =  c + screenMode(s,d);\t\t\n\n\telse if(blendingMode == 22)\n\t\tc =  c + softLight(s,d);\n\n\telse if(blendingMode == 23)\n\t\tc =  c + spotlightBlend(s,d);\n\n\telse if(blendingMode == 24)\n\t\tc =  c + substract(s,d);\n\n\telse if(blendingMode == 25)\n\t\tc =  c + vividLight(s,d);\n\n\t// grabbing the result\n\tvec4 result = vec4(c.rgb, s.a);\n\n\n\n\n\n\t// multiplying the result by the opacity\n\tfragColor = mix(d, result, opacity);\n}\t")
+        param.setValue("""//                                                
+//
+//                          MMMMMMMMMMMMMMMMMMMMMMMMMMMM
+//                        MM.                          .MM
+//                       MM.  .MMMMMMMMMMMMMMMMMMMMMM.  .MM
+//                      MM.  .MMMMMMMMMMMMMMMMMMMMMMMM.  .MM
+//                     MM.  .MMMM        MMMMMMM    MMM.  .MM
+//                    MM.  .MMM           MMMMMM     MMM.  .MM
+//                   MM.  .MmM              MMMM      MMM.  .MM
+//                  MM.  .MMM                 MM       MMM.  .MM
+//                 MM.  .MMM                   M        MMM.  .MM
+//                MM.  .MMM                              MMM.  .MM
+//                 MM.  .MMM                            MMM.  .MM
+//                  MM.  .MMM       M                  MMM.  .MM
+//                   MM.  .MMM      MM                MMM.  .MM
+//                    MM.  .MMM     MMM              MMM.  .MM
+//                     MM.  .MMM    MMMM            MMM.  .MM
+//                      MM.  .MMMMMMMMMMMMMMMMMMMMMMMM.  .MM
+//                       MM.  .MMMMMMMMMMMMMMMMMMMMMM.  .MM
+//                        MM.                          .MM
+//                          MMMMMMMMMMMMMMMMMMMMMMMMMMMM
+//
+//
+// Adaptation pour Natron par F. Fernandez
+// Code original : crok_logicop Matchbox pour Autodesk Flame
+//
+// Adapted to Natron by F.Fernandez
+// Original code : crok_logicop Matchbox for Autodesk Flame
+//
+//****************************************************************************/
+//
+// Filename: logicOPS_3vis.glsl
+// Author: Eric Pouliot
+//
+// Copyright (c) 2013 3vis, Inc.
+//
+//*****************************************************************************/
+//
+//  License Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+//
+//  25 of the layer blending modes from Photoshop.
+//
+//  The ones I couldn't figure out are from Nvidia's advanced blend equations extension spec -
+//  http://www.opengl.org/registry/specs/NV/blend_equation_advanced.txt
+//
+//  ~bj.2013
+//
+//*****************************************************************************/
+
+// iChannel0: B, filter = nearest
+// iChannel1: A, filter = nearest
+// iChannel1: Modulate, filter = nearest
+// BBox: iChannel0
+
+
+const vec2 iChannelOffset[4] = vec2[4]( vec2(0.,0.), vec2(0.,0.), vec2(0.,0.), vec2(0.,0.) );
+
+uniform int blendingMode = 17; // Operation : (blending mode), min=0., max=25.
+uniform float opacity = 1; // Mix : (mix), min=0., max=1.
+
+uniform bool Ar = true; // R
+uniform bool Ag = true; // G
+uniform bool Ab = true; // B
+uniform bool Aa = true; // A
+
+uniform bool Br = true; // R
+uniform bool Bg = true; // G
+uniform bool Bb = true; // B
+uniform bool Ba = true; // A
+
+
+//********************* fonctions ***********************//
+
+float overlay( float s, float d )
+{
+  return (d < 0.5) ? 2.0 * s * d : 1.0 - 2.0 * (1.0 - s) * (1.0 - d);
+}
+
+float softLight( float s, float d )
+{
+  return (s < 0.5) ? d - (1.0 - 2.0 * s) * d * (1.0 - d)
+    : (d < 0.25) ? d + (2.0 * s - 1.0) * d * ((16.0 * d - 12.0) * d + 3.0)
+           : d + (2.0 * s - 1.0) * (sqrt(d) - d);
+}
+
+float hardLight( float s, float d )
+{
+  return (s < 0.5) ? 2.0 * s * d : 1.0 - 2.0 * (1.0 - s) * (1.0 - d);
+}
+
+float vividLight( float s, float d )
+{
+  return (s < 0.5) ? 1.0 - (1.0 - d) / (2.0 * s) : d / (2.0 * (1.0 - s));
+}
+
+float pinLight( float s, float d )
+{
+  return (2.0 * s - 1.0 > d) ? 2.0 * s - 1.0 : (s < 0.5 * d) ? 2.0 * s : d;
+}
+
+
+
+//****************** modes de fusion ********************//
+
+vec4 colorBurn( vec4 s, vec4 d )
+{
+  return 1.0 - (1.0 - d) / s;
+}
+
+vec4 colorDodge( vec4 s, vec4 d )
+{
+  return d / (1.0 - s);
+}
+
+vec4 darken( vec4 s, vec4 d )
+{
+  vec4 c;
+  c = min(s,d);
+  return c;
+}
+
+vec4 difference( vec4 s, vec4 d )
+{
+  return abs(d - s);
+}
+
+vec4 darkerColor( vec4 s, vec4 d )
+{
+  return (s.x + s.y + s.z < d.x + d.y + d.z) ? s : d;
+}
+
+vec4 divide( vec4 s, vec4 d )
+{
+  return s / d;
+}
+
+vec4 exclusion( vec4 s, vec4 d )
+{
+  return s + d - 2.0 * s * d;
+}
+
+vec4 hardLight( vec4 s, vec4 d )
+{
+  vec4 c;
+  c.x = hardLight(s.x,d.x);
+  c.y = hardLight(s.y,d.y);
+  c.z = hardLight(s.z,d.z);
+  return c;
+}
+
+vec4 hardMix( vec4 s, vec4 d )
+{
+  return floor(s + d);
+}
+
+vec4 lighten( vec4 s, vec4 d )
+{
+  return max(s,d);
+}
+
+vec4 lighterColor( vec4 s, vec4 d )
+{
+  return (s.x + s.y + s.z > d.x + d.y + d.z) ? s : d;
+}
+
+vec4 linearBurn( vec4 s, vec4 d )
+{
+  return s + d - 1.0;
+}
+
+vec4 linearDodge( vec4 s, vec4 d )
+{
+  return s + d;
+}
+
+
+vec4 linearLight( vec4 s, vec4 d )
+{
+  return 2.0 * s + d - 1.0;
+}
+
+vec4 screen( vec4 s, vec4 d )
+{
+  return s + d - s * d;
+}
+
+vec4 screenMode( vec4 s, vec4 d )
+{
+  vec4 c;
+  c = 1 - (1 - s) * (1 - d);
+  return c;
+}
+
+vec4 matte( vec4 s, vec4 d)
+{
+  vec4 c = vec4(0,0,0,0);
+  c = (s*s.a) + (d*(1-s.a));
+  c.a = s.a;
+  return c;
+}
+
+vec4 multiply( vec4 s, vec4 d )
+{
+  return s*d;
+}
+
+vec4 overlay( vec4 s, vec4 d )
+{
+  vec4 c;
+  c.x = overlay(s.x,d.x);
+  c.y = overlay(s.y,d.y);
+  c.z = overlay(s.z,d.z);
+  return c;
+}
+
+vec4 pinLight( vec4 s, vec4 d )
+{
+  vec4 c;
+  c.x = pinLight(s.x,d.x);
+  c.y = pinLight(s.y,d.y);
+  c.z = pinLight(s.z,d.z);
+  return c;
+}
+
+vec4 plus( vec4 s, vec4 d )
+{
+  return s + d;
+}
+
+vec4 softLight( vec4 s, vec4 d )
+{
+  vec4 c;
+  c.x = softLight(s.x,d.x);
+  c.y = softLight(s.y,d.y);
+  c.z = softLight(s.z,d.z);
+  return c;
+}
+
+vec4 spotlightBlend( vec4 s, vec4 d )
+{
+  return d*s+d;
+}
+
+vec4 substract( vec4 s, vec4 d )
+{
+  return s - d;
+}
+
+vec4 vividLight( vec4 s, vec4 d )
+{
+  vec4 c;
+  c.x = vividLight(s.x,d.x);
+  c.y = vividLight(s.y,d.y);
+  c.z = vividLight(s.z,d.z);
+  return c;
+}
+
+vec4 luminosity( vec4 s, vec4 d )
+{
+  float dLum = dot(d, vec4(0.3, 0.59, 0.11,0));
+  float sLum = dot(s, vec4(0.3, 0.59, 0.11,0));
+  float lum = sLum - dLum;
+  vec4 c = d + lum;
+  float minC = min(min(c.x, c.y), c.z);
+  float maxC = max(max(c.x, c.y), c.z);
+  if(minC < 0.0) return sLum + ((c - sLum) * sLum) / (sLum - minC);
+  else if(maxC > 1.0) return sLum + ((c - sLum) * (1.0 - sLum)) / (maxC - sLum);
+  else return c;
+}
+
+vec4 over( vec4 s, vec4 d )
+{
+  vec4 c;
+  c = s + ( d*(1-s.a) );
+  return c;
+}
+
+
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+  // source texture (upper layer)
+  vec4 s = texture2D(iChannel1, (fragCoord.xy-iChannelOffset[1].xy)/iChannelResolution[1].xy);
+
+  // destination texture (lower layer)
+  vec4 d = texture2D(iChannel0, fragCoord.xy / iChannelResolution[0].xy);
+
+  // mask texture (mask entry)
+  vec4 mask = texture2D(iChannel2, (fragCoord.xy-iChannelOffset[2].xy)/iChannelResolution[2].xy);
+
+  vec4 c = vec4(0.0);
+
+
+  // ########### selection des couches ########### //
+
+  if(Ar == false)
+    s.r = 0;
+
+  if(Ag == false)
+    s.g = 0;
+
+  if(Ab == false)
+    s.b = 0;
+
+  if(Aa == false)
+    s.a = 0;
+
+  if(Br == false)
+    d.r = 0;
+
+  if(Bg == false)
+    d.g = 0;
+
+  if(Bb == false)
+    d.b = 0;
+
+  if(Ba == false)
+    d.a = 0;
+
+  // ########### selection des modes de fusion ########### //
+
+  if( blendingMode == 0)
+    c =  c + colorBurn(s,d);
+
+  else if( blendingMode == 1)
+    c =  c + colorDodge(s,d);
+
+  else if( blendingMode == 2)
+    c =  c + darken(s,d);
+
+  else if(blendingMode == 3)
+    c =  c + darkerColor(s,d);
+
+  else if(blendingMode == 4)
+    c =  c + difference(s,d);
+
+  else if(blendingMode == 5)
+    c =  c + divide(s,d);
+
+  else if(blendingMode == 6)
+    c =  c + exclusion(s,d);
+
+  else if(blendingMode == 7)
+    c =  c + hardLight(s,d);
+
+  else if(blendingMode == 8)
+    c =  c + hardMix(s,d);
+
+  else if(blendingMode == 9)
+    c =  c + lighten(s,d);
+
+  else if(blendingMode == 10)
+    c =  c + lighterColor(s,d);
+
+  else if(blendingMode == 11)
+    c =  c + linearBurn(s,d);
+
+  else if(blendingMode == 12)
+    c =  c + linearDodge(s,d);
+
+  else if(blendingMode == 13)
+    c =  c + linearLight(s,d);
+
+  else if(blendingMode == 14)
+    c =  c + luminosity(s,d);
+
+  else if(blendingMode == 15)
+    c =  c + matte(s,d);
+
+  else if(blendingMode == 16)
+    c =  c + multiply(s,d);
+
+  else if(blendingMode == 17)
+    c =  c + over(s,d);
+
+  else if(blendingMode == 18)
+    c =  c + overlay(s,d);
+
+  else if(blendingMode == 19)
+    c =  c + pinLight(s,d);
+
+  else if(blendingMode == 20)
+    c =  c + plus(s,d);
+
+  else if(blendingMode == 21)
+    c =  c + screenMode(s,d);
+
+  else if(blendingMode == 22)
+    c =  c + softLight(s,d);
+
+  else if(blendingMode == 23)
+    c =  c + spotlightBlend(s,d);
+
+  else if(blendingMode == 24)
+    c =  c + substract(s,d);
+
+  else if(blendingMode == 25)
+    c =  c + vividLight(s,d);
+
+  // grabbing the result
+  vec4 result = vec4(c.rgb, s.a);
+
+  // multiplying the result by the opacity
+  fragColor = mix(d, result, opacity);
+}""")
         del param
 
     param = lastNode.getParam("mipmap0")
